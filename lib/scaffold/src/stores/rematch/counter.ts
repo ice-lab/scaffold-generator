b6a95a689da292a095ab623fb9808cd66e8b52ab
yangfan <18767120422@163.com>
2020-10-30 17:27:23 +0800

    feat: update lint

--- lib/scaffold/src/stores/rematch/counter.ts
+++ lib/scaffold/src/stores/rematch/counter.ts
@@ -1,16 +1,15 @@
+/* eslint-disable import/no-cycle */
 import { createModel } from '@rematch/core';
 import { Dispatch } from './index';
 
-export type CounterState = number
+export type CounterState = number;
 
-const delay = (ms: number) =>
-  new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 const model = {
   state: 0,
   reducers: {
-    increment: (state: CounterState, payload: number): CounterState =>
-      state + payload,
+    increment: (state: CounterState, payload: number): CounterState => state + payload,
   },
   effects: (dispatch: Dispatch) => ({
     async incrementAsync(payload = 1) {
